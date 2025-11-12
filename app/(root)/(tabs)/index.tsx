2cbc4ba8254348d689e29e822245db575c14aaa5
samedyhunx <samedy.hunx@gmail.com>
2025-11-12 12:53:02 +0700

    feature/home: homepage done

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,7 +1,9 @@
+import { Card, FeatuedCard } from "@/components/Cards";
+import { Filters } from "@/components/Filters";
 import { Search } from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
-import { Image, Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
@@ -22,8 +24,44 @@ export default function Index() {
           </View>
           <Image source={icons.bell} className="size-6" />
         </View>
+        <Search />
+        <View className="my-5">
+          <View className="flex flex-row items-center justify-between">
+            <Text className="text-xl font-rubik-bold text-black-300">
+              Featured
+            </Text>
+            <TouchableOpacity>
+              <Text className="text-base font-rubik-bold text-primary-300">
+                See All
+              </Text>
+            </TouchableOpacity>
+          </View>
+
+          <View className="flex flex-row gap-5 mt-5">
+            <FeatuedCard />
+            <FeatuedCard />
+            <FeatuedCard />
+          </View>
+        </View>
+
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold text-black-300">
+            Our Recommendation
+          </Text>
+          <TouchableOpacity>
+            <Text className="text-base font-rubik-bold text-primary-300">
+              See All
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <Filters />
+
+        <View className="flex flex-row gap-5 mt-5">
+          <Card />
+          <Card />
+        </View>
       </View>
-      <Search />
     </SafeAreaView>
   );
 }
